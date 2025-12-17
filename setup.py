512314108c6df391499cd096fc5e8cfa6a32c242
Chimdinyelum <chyjohn777@gmail.com>
2025-12-17 09:11:24 +0100

    Fix: Using the correct setup.py conf

--- setup.py
+++ setup.py
@@ -2,11 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name="my-shared-utils",
-    version="0.1.0",
-    packages=find_packages(),
+    version="1.0.1",
+    # packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    # package_dir={'my_shared_utils': 'src/my_shared_utils'},
+    # packages=['my_shared_utils'],
     install_requires=[
         # List your dependencies here
+        "python-dotenv>=1.1.1",
+        "mysql-connector-python>=9.4.0",
     ],
-    author="Your Name",
+    # py_modules=[],  # No top-level modules
+    author="Joe",
     description="Shared utilities for Python projects",
 )
