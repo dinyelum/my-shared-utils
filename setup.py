cd7eae3581e2cda93e1c75b843ed08dc6372216f
Chimdinyelum <chyjohn777@gmail.com>
2025-12-18 17:51:42 +0100

    Fix: Removed mysql-connector-python from dependependencies

--- setup.py
+++ setup.py
@@ -2,16 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="my-shared-utils",
-    version="1.0.1",
+    version="1.0.2",
     # packages=find_packages(),
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    # package_dir={'my_shared_utils': 'src/my_shared_utils'},
-    # packages=['my_shared_utils'],
     install_requires=[
         # List your dependencies here
         "python-dotenv>=1.1.1",
-        "mysql-connector-python>=9.4.0",
+        "mysqlclient>=2.2.7"
+        # "mysql-connector-python>=9.4.0",
     ],
     # py_modules=[],  # No top-level modules
     author="Joe",
