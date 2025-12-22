76a3f6bafd5820c83120be889f5db1cd9d30a79b
Chimdinyelum <chyjohn777@gmail.com>
2025-12-22 07:01:09 +0100

    Fix: Package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="my-shared-utils",
-    version="1.0.3",
+    version="1.1.0",
     # packages=find_packages(),
     packages=find_packages(where="src"),
     package_dir={"": "src"},
